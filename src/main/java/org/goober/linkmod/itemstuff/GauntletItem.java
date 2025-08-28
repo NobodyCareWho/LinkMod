000169c9b992b520efc9141787ceb83d858e3a61
Link <109550379+NobodyCareWho@users.noreply.github.com>
2025-08-28 15:19:49 -0400

    started work on setting up gauntlet to be able to use custom enchantments, and added a projectile texture

--- src/main/java/org/goober/linkmod/itemstuff/GauntletItem.java
+++ src/main/java/org/goober/linkmod/itemstuff/GauntletItem.java
@@ -1,7 +1,9 @@
 package org.goober.linkmod.itemstuff;
 
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.AttributeModifierSlot;
 import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
@@ -9,8 +11,10 @@ import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.UseAction;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.world.World;
 
 public class GauntletItem extends Item {
 
@@ -23,6 +27,18 @@ public class GauntletItem extends Item {
                 .add(EntityAttributes.ATTACK_DAMAGE, new EntityAttributeModifier(BASE_ATTACK_DAMAGE_MODIFIER_ID, 3.0, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
                 .add(EntityAttributes.ATTACK_SPEED, new EntityAttributeModifier(BASE_ATTACK_SPEED_MODIFIER_ID, 1.5, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
                 .build();
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+            return UseAction.BOW;
+    }
+
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return 72000;
+    }
+
+    public boolean onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        return true;
     }
 
     public void postHit(ItemStack stack,
