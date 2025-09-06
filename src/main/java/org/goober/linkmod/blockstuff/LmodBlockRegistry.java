c86636b2efd3cff55d2f1f61d566360fc0bc01fc
Link <109550379+NobodyCareWho@users.noreply.github.com>
2025-09-05 22:11:12 -0400

    Added Hematite Blocksets, fixed desperado spawn egg texture to reflect mob colors, added hematite blocksets, and made desperado extortioner and stalker spawn in raids in their respective biomes.

--- src/main/java/org/goober/linkmod/blockstuff/LmodBlockRegistry.java
+++ src/main/java/org/goober/linkmod/blockstuff/LmodBlockRegistry.java
@@ -18,10 +18,56 @@ public class LmodBlockRegistry {
             settings -> new FlowerBlock(StatusEffects.SATURATION, 0.1F, settings),
             AbstractBlock.Settings.copy(Blocks.POPPY));
     // Flower constructors need effect parameters for some reason? That's what the saturation effect is.
-    public static final Block ROUNDED_SANDSTONE = register("rounded_sandstone", Block::new, Block.Settings.create().mapColor(MapColor.PALE_YELLOW).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.5F, 6.0F));
+    public static final Block ROUNDED_SANDSTONE = register("rounded_sandstone", Block::new, Block.Settings.create().mapColor(MapColor.PALE_YELLOW).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.STONE));
     public static final Block GILDED_SANDSTONE = register("gilded_sandstone", Block::new, Block.Settings.create().mapColor(MapColor.PALE_YELLOW).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.5F, 6.0F));
+
+    public static final Block HEMATITE = register("hematite", Block::new, Block.Settings.create().mapColor(MapColor.DULL_RED).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.IRON));
+    public static final Block HEMATITE_STAIRS = register("hematite_stairs",
+            state -> new StairsBlock(HEMATITE.getDefaultState(), state),
+            Block.Settings.copy(HEMATITE)
+    );
+    public static final Block HEMATITE_SLAB = register("hematite_slab",
+            SlabBlock::new,
+            Block.Settings.copy(HEMATITE)
+    );
+    public static final Block HEMATITE_WALL = register("hematite_wall",
+            WallBlock::new,
+            Block.Settings.copy(HEMATITE)
+    );
+
+    public static final Block POLISHED_HEMATITE = register("polished_hematite", Block::new, Block.Settings.create().mapColor(MapColor.DULL_RED).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.IRON));
+    public static final Block POLISHED_HEMATITE_STAIRS = register("polished_hematite_stairs",
+            state -> new StairsBlock(POLISHED_HEMATITE.getDefaultState(), state),
+            Block.Settings.copy(POLISHED_HEMATITE)
+    );
+    public static final Block POLISHED_HEMATITE_SLAB = register("polished_hematite_slab",
+            SlabBlock::new,
+            Block.Settings.copy(POLISHED_HEMATITE)
+    );
+    public static final Block POLISHED_HEMATITE_WALL = register("polished_hematite_wall",
+            WallBlock::new,
+            Block.Settings.copy(POLISHED_HEMATITE)
+    );
+
+    public static final Block HEMATITE_BRICKS = register("hematite_bricks", Block::new, Block.Settings.create().mapColor(MapColor.DULL_RED).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.IRON));
+    public static final Block HEMATITE_BRICK_STAIRS = register("hematite_brick_stairs",
+            state -> new StairsBlock(HEMATITE_BRICKS.getDefaultState(), state),
+            Block.Settings.copy(HEMATITE_BRICKS)
+    );
+    public static final Block HEMATITE_BRICK_SLAB = register("hematite_brick_slab",
+            SlabBlock::new,
+            Block.Settings.copy(HEMATITE_BRICKS)
+    );
+    public static final Block HEMATITE_BRICK_WALL = register("hematite_brick_wall",
+            WallBlock::new,
+            Block.Settings.copy(HEMATITE_BRICKS)
+    );
+
+    public static final Block CHISELED_HEMATITE = register("chiseled_hematite", Block::new, Block.Settings.create().mapColor(MapColor.DULL_RED).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(1.5F, 6.0F).sounds(BlockSoundGroup.IRON));
+
     public static final Block LATHE = register("lathe", LatheBlock::new, Block.Settings.create().mapColor(MapColor.DARK_AQUA).requiresTool().strength(1.5F, 6.0F));
     public static final Block EXP_CHEST = register("exp_chest", ExpChestBlock::new, Block.Settings.create().mapColor(MapColor.PALE_PURPLE).instrument(NoteBlockInstrument.BASS).requiresTool().strength(2.5F, 9900).sounds(BlockSoundGroup.STONE).luminance(state -> 7));
+
 
     private static Block register(String path, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
         final Identifier identifier = Identifier.of("lmod", path);
--- src/main/java/org/goober/linkmod/mixin/RaidMixin.java
+++ src/main/java/org/goober/linkmod/mixin/RaidMixin.java
@@ -2,19 +2,53 @@ package org.goober.linkmod.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.raid.RaiderEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.village.raid.Raid;
+import org.goober.linkmod.entitystuff.AgentPillagerEntity;
+import org.goober.linkmod.entitystuff.DesperadoEntity;
 import org.goober.linkmod.entitystuff.LmodEntityRegistry;
+import org.goober.linkmod.entitystuff.StalkerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.minecraft.command.EntitySelectorReader.RANDOM;
 
 @Mixin(Raid.class)
 public class RaidMixin {
-    
-    @Inject(method = "<clinit>", at = @At("TAIL"))
-    private static void addCustomPillagers(CallbackInfo ci) {
-        // your gonna need to modify the enum for ur pillagers btw, pillagr stuff go here
-        // example spawn counts array: new int[]{0, 2, 1, 2, 3, 3, 4, 2}
+    @Inject(method = "addRaider", at = @At("HEAD"), cancellable = true)
+    private void injectDesperado(ServerWorld world, int wave, RaiderEntity raider, BlockPos pos, boolean existing, CallbackInfo ci) {
+        if (raider.getType() == EntityType.PILLAGER) {
+            float temp = world.getBiome(pos).value().getTemperature();
+            RaiderEntity replacement;
+            if (raider.getRandom().nextFloat() < 0.3F) {
+                if (temp < 0.3F) {
+                    // Cold biome replacement
+                    replacement = new StalkerEntity(LmodEntityRegistry.STALKER, world);
+                } else if (temp < 1.0F) {
+                    // Normal biome replacement
+                    replacement = new AgentPillagerEntity(LmodEntityRegistry.AGENTPILLAGER, world);
+                } else {
+                    // Hot biome replacement
+                    replacement = new DesperadoEntity(LmodEntityRegistry.DESPERADO, world);
+                }
+
+                replacement.refreshPositionAndAngles(
+                        raider.getX(),
+                        raider.getY(),
+                        raider.getZ(),
+                        raider.getYaw(),
+                        raider.getPitch()
+                );
+
+            ((Raid)(Object)this).addRaider(world, wave, replacement, pos, existing);
+
+            raider.discard(); // remove vanilla pillager
+            ci.cancel();
+            }
+        }
     }
 }
